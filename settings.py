ec693a34832eee26b62573c937c73f8fec6933e2
magrhino <magrhino@gmail.com>
2025-10-28 17:22:58 -0500

    Add new settings for rastastugan and homiehelpdesk

--- settings.py
+++ settings.py
@@ -20,6 +20,8 @@ class Settings:
                 "lst": "",
                 "ulcx": "",
                 "onlyencodes": "",
+                "rastastugan": "",
+                "homiehelpdesk": "",
             },
             "gg_path": "",  # Path to GG-Bot e.g. /home/user/gg-bot-upload-assistant/ --- Not required only for export_gg_bot()
             "ua_path": "",  # Path to upload-assistant, e.g. /home/user/uplaad-assistant/ --- Optional
@@ -57,6 +59,8 @@ class Settings:
             "upload.cx": "ulcx",
             "onlyencodes": "onlyencodes",
             "oe": "onlyencodes",
+            "ras": "rastastugan",
+            "hhd": "homiehelpdesk",
         }
 
         # Basic hierarchy for qualities used to see if a file is an upgrade
